35519e5f650cf6df38bd98b1f4e094cedb40a537
augustonildo <augustocarvalhopp@gmail.com>
2022-10-28 20:27:24 -0300

    Server file from introduction classes

--- server.c
+++ server.c
@@ -1,6 +1,62 @@
+#include "common.h"
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    printf("Server");
-    return 0;
+#define BUFSZ 1024
+
+int main(int argc, char *argv[])
+{
+  struct sockaddr_storage storage;
+  server_sockaddr_init(argv[1], argv[2], &storage);
+
+  int s = socket(storage.ss_family, SOCK_STREAM, 0);
+  if (s == -1)
+  {
+    logexit("socket");
+  }
+
+  struct sockaddr *addr = (struct sockaddr *)(&storage);
+  if (0 != bind(s, addr, sizeof(storage)))
+  {
+    logexit("bind");
+  }
+
+  if (0 != listen(s, 10))
+  {
+    logexit("listen");
+  }
+
+  char addrstr[BUFSZ];
+  addrtostr(addr, addrstr, BUFSZ);
+
+  while (1)
+  {
+    struct sockaddr_storage cstorage;
+    struct sockaddr *caddr = (struct sockaddr *)(&cstorage);
+    socklen_t caddrlen = sizeof(cstorage);
+
+    int csock = accept(s, caddr, &caddrlen);
+    if (0 != csock)
+    {
+      logexit("accept");
+    }
+
+    char caddrstr[BUFSZ];
+    addrtostr(caddr, caddrstr, BUFSZ);
+
+    char buf[BUFSZ];
+    memset(buf, 0, BUFSZ);
+    size_t count = recv(csock, buf, BUFSZ - 1, 0);
+
+    count = send(csock, buf, strlen(buf) + 1, 0);
+    if (count != strlen(buf) + 1)
+    {
+      logexit("send");
+    }
+    close(csock);
+  }
+  exit(EXIT_SUCCESS);
 }
